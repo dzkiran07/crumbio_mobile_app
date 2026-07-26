f64d687bb3a15e984b5188dba49ad6b5eec6c432
dzkiran07 <kasalawotkiran2@gmail.com>
2026-07-26 17:26:28 +0545

    Add ProductEntity class to represent marketplace product details

---
+++ lib/features/marketplace/domain/entities/product_entity.dart
@@ -0,0 +1,21 @@
+class ProductEntity {
+	final String id;
+	final String name;
+	final double basePrice;
+	final String image;
+	final String category;
+	final String description;
+	final String bakerName;
+	final String availability;
+
+	ProductEntity({
+		required this.id,
+		required this.name,
+		required this.basePrice,
+		required this.image,
+		required this.category,
+		required this.description,
+		required this.bakerName,
+		required this.availability,
+	});
+}
